a475d02709af5117e323841b0a4b4fab9abe5d69
Andrea Bellver <bellverrha@gmail.com>
2022-12-19 22:12:54 +0100

    Segundo ejercicio JS OBC

---
+++ index.js
@@ -0,0 +1,19 @@
+const myName = "Andrea Bellver García";
+let age = 21;
+let amIDeveloper = false;
+const birthDate = new Date(2001, 4, 21, 18, 42);
+let myFavoriteBook = {
+    title: "Thus Spoke Zarathustra",
+    author: "Friedrich Nietzsche",
+    publication: 1883,
+    url: "https://es.wikipedia.org/wiki/As%C3%AD_habl%C3%B3_Zaratustra",
+};
+
+const list = 
+    [
+        myName,
+        age,
+        amIDeveloper,
+        birthDate,
+        myFavoriteBook
+    ];
